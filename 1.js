d1221d6c6e4ee24bce3ae82b817d558456a72ce3
Татьяна Пятыхина <tpyatyhina@mail.ru>
2021-07-04 14:14:21 +0300

    bubble

--- 1.js
+++ 1.js
@@ -1,1 +1,14 @@
-console.log('Algorithms');
+function bubbleSort(array) {
+    for(let i = 0; i < array.length-1; i++) {
+        for (let j = i; j< array.length; j++) {
+            if (array[i] > array[j]) {
+                let tmp = array[i];
+                array[i] = array[j];
+                array[j] = tmp;
+            }
+        }
+    }
+    return array;
+}
+
+console.log('bubbleSort([3, 5, 6, 0, 9, 1, 4])', bubbleSort([3, 5, 6, 0, 9, 1, 4]))
